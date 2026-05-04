48a62e7dfe3cba093e88b3fa31890f8883bd36ab
marckstar-RjJ <202001810@est.umss.edu>
2026-05-03 21:58:49 -0400

    Commit 2: Inclusión de proyecto Visual Studio y archivo DACPAC final

---
+++ Proyecto_Celulares_DACPAC/dbo/Tables/Categorias.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Categorias] (
+    [CategoriaID] INT          IDENTITY (1, 1) NOT NULL,
+    [Nombre]      VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([CategoriaID] ASC)
+);
+
---
+++ Proyecto_Celulares_DACPAC/dbo/Tables/Tecnicos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Tecnicos] (
+    [TecnicoID]    INT           IDENTITY (1, 1) NOT NULL,
+    [Nombre]       VARCHAR (100) NOT NULL,
+    [Especialidad] VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([TecnicoID] ASC)
+);
+
